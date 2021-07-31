0e9585c242f56fa6fb9735ba5eb43722a46f6b7a
Santi Sabate <ssabate@iesebre.com>
2021-07-31 19:59:13 +0200

    Vista vacía

---
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -0,0 +1,32 @@
+package com.vaadin.tutorial.crm.ui;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+/**
+ * A sample Vaadin view class.
+ * <p>
+ * To implement a Vaadin view just extend any Vaadin component and
+ * use @Route annotation to announce it in a URL as a Spring managed
+ * bean.
+ * Use the @PWA annotation make the application installable on phones,
+ * tablets and some desktop browsers.
+ * <p>
+ * A new instance of this class is created for every new user and every
+ * browser tab/window.
+ */
+@Route("")
+public class MainView extends VerticalLayout {
+
+    /**
+     * Construct a new Vaadin view.
+     * <p>
+     * Build the initial UI state for the user accessing the application.
+     *
+     */
+    public MainView() {
+
+
+    }
+
+}
