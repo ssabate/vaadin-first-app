8f1195921c8794af3045846a069ec0a5d4d63e46
Santi Sabate <ssabate@iesebre.com>
2021-08-04 12:09:41 +0200

    Contact grid created but not yet finished

--- src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -1,7 +1,10 @@
 package com.vaadin.tutorial.crm.ui;
 
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadin.tutorial.crm.backend.entity.Contact;
+import com.vaadin.tutorial.crm.backend.service.ContactService;
 
 /**
  * A sample Vaadin view class.
@@ -18,15 +21,28 @@ import com.vaadin.flow.router.Route;
 @Route("")
 public class MainView extends VerticalLayout {
 
-    /**
-     * Construct a new Vaadin view.
-     * <p>
-     * Build the initial UI state for the user accessing the application.
-     *
-     */
-    public MainView() {
+    private ContactService contactService;
+    private Grid<Contact> grid = new Grid<>(Contact.class);
 
+    public MainView(ContactService contactService) {
+        this.contactService=contactService;
+        addClassName("list-view");
+        setSizeFull();
+        configureGrid();
+        add(grid);
+        updateList();
+    }
 
+    private void configureGrid() {
+        grid.addClassName("contact-grid");
+        grid.setSizeFull();
+        grid.setColumns("firstName", "lastName", "email", "status");
+    }
+
+    private void updateList() {
+        grid.setItems(contactService.findAll());
     }
 
 }
+
+
