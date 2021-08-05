6a4c38b28d82ffbcec1a8f1c0fff743a93225bb7
Santi Sabate <ssabate@iesebre.com>
2021-08-05 23:40:14 +0200

    Contact grid finished

--- src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -3,6 +3,7 @@ package com.vaadin.tutorial.crm.ui;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadin.tutorial.crm.backend.entity.Company;
 import com.vaadin.tutorial.crm.backend.entity.Contact;
 import com.vaadin.tutorial.crm.backend.service.ContactService;
 
@@ -36,7 +37,13 @@ public class MainView extends VerticalLayout {
     private void configureGrid() {
         grid.addClassName("contact-grid");
         grid.setSizeFull();
+        grid.removeColumnByKey("company");
         grid.setColumns("firstName", "lastName", "email", "status");
+        grid.addColumn(contact -> {
+            Company company = contact.getCompany();
+            return company == null ? "-" : company.getName();
+        }).setHeader("Company");
+        grid.getColumns().forEach(col -> col.setAutoWidth(true));
     }
 
     private void updateList() {
