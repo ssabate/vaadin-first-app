7995bd1bdf460761f5735f60c051c9760b87bcc1
Santi Sabate <ssabate@iesebre.com>
2021-08-08 10:09:41 +0200

    Layout applied but form without action

--- src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -1,6 +1,8 @@
 package com.vaadin.tutorial.crm.ui;
 
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
@@ -22,19 +24,28 @@ import com.vaadin.tutorial.crm.backend.service.ContactService;
  * browser tab/window.
  */
 @Route("")
+@CssImport("./styles/shared-styles.css")
 public class MainView extends VerticalLayout {
 
     private ContactService contactService;
     private Grid<Contact> grid = new Grid<>(Contact.class);
     private TextField filterText=new TextField();
+    private ContactForm form;
     
     public MainView(ContactService contactService) {
         this.contactService=contactService;
         addClassName("list-view");
         setSizeFull();
+        
         configureFilter();
         configureGrid();
-        add(filterText);
+        
+        form=new ContactForm();
+        Div content=new Div(grid, form);
+        content.addClassName("content");
+        content.setSizeFull();
+        
+        add(filterText, content);
         add(grid);
         updateList();
     }
@@ -59,7 +70,7 @@ public class MainView extends VerticalLayout {
     }
 
     private void updateList() {
-        grid.setItems(contactService.findAll(filterText.getValue().strip()));
+        grid.setItems(contactService.findAll(filterText.getValue().trim()));
     }
 
 }
